f73b5b2905367a49301c7269441b1ed8a314e806
Nigel Small <nasmall@pm.me>
2021-08-05 16:39:19 +0100

    Version 2021.0.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_readme():
 
 setup(
     name="interchange",
-    version="2021.0.1",
+    version="2021.0.2",
     description="Data types and interchange formats",
     author="Nigel Small",
     author_email="technige@py2neo.org",
